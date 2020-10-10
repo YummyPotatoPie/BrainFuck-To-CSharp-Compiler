61cc967a9c15b9e8bfc2a4bac3ae91d969f32d2b
YummyPotatoPie <53563234+YummyPotatoPie@users.noreply.github.com>
2020-10-10 12:27:01 +0300

    UTM.bf compiled file

---
+++ out.cs
@@ -0,0 +1,476 @@
+class Program {
+	static public void Main() {
+		int[] Mem = new int[30000]; //Memory cells
+		for(int i = 0; i < 30000; i++)
+			Mem[i] = 0;
+		int Ptr = 0; //Pointer
+
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		Ptr++;
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		Ptr++;
+		Ptr++;
+		Ptr++;
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		while(Mem[Ptr] > 0) {
+			Ptr++;
+			Ptr++;
+			Mem[Ptr] = System.Console.Read();
+			while(Mem[Ptr] > 0) {
+				Ptr++;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Ptr--;
+				while(Mem[Ptr] > 0) {
+					while(Mem[Ptr] > 0) {
+						Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+						Ptr++;
+					}
+					Ptr--;
+					Ptr--;
+				}
+				Ptr--;
+				while(Mem[Ptr] > 0) {
+					Ptr++;
+				}
+				Ptr++;
+			}
+			Ptr++;
+			Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+			while(Mem[Ptr] > 0) {
+				Ptr--;
+				Ptr--;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Ptr++;
+				Ptr++;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				while(Mem[Ptr] > 0) {
+					Ptr--;
+					Ptr--;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+					Ptr++;
+					Ptr++;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+					while(Mem[Ptr] > 0) {
+						Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+						Ptr++;
+					}
+					Ptr--;
+				}
+			}
+			Ptr--;
+			while(Mem[Ptr] > 0) {
+				Ptr--;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				Ptr--;
+				while(Mem[Ptr] > 0) {
+					Ptr--;
+				}
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Ptr--;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				while(Mem[Ptr] > 0) {
+					Ptr++;
+				}
+				Ptr--;
+				Ptr--;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Ptr++;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				Ptr++;
+				Ptr++;
+				Ptr++;
+			}
+			Ptr--;
+		}
+		Ptr--;
+		while(Mem[Ptr] > 0) {
+			Ptr--;
+		}
+		Ptr++;
+		while(Mem[Ptr] > 0) {
+			Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+			while(Mem[Ptr] > 0) {
+				Ptr++;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Ptr--;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+			}
+			Ptr++;
+			while(Mem[Ptr] > 0) {
+				Ptr--;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Ptr++;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+			}
+			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+			Ptr--;
+			Ptr--;
+			Ptr--;
+			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+			Ptr++;
+			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+			Ptr++;
+			while(Mem[Ptr] > 0) {
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				while(Mem[Ptr] > 0) {
+					Ptr--;
+					Ptr--;
+					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+					Ptr++;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+					Ptr++;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+					while(Mem[Ptr] > 0) {
+						Ptr--;
+						Ptr--;
+						while(Mem[Ptr] > 0) {
+							Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+						}
+						Ptr++;
+						Ptr++;
+						Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+						while(Mem[Ptr] > 0) {
+							Ptr--;
+							Ptr--;
+							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+							Ptr++;
+							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+							Ptr++;
+							Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+							while(Mem[Ptr] > 0) {
+								Ptr--;
+								Ptr--;
+								Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+								Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+								Ptr++;
+								Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+								Ptr++;
+								Ptr++;
+								Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+								Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+								Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+								Ptr--;
+								Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+								while(Mem[Ptr] > 0) {
+									Ptr--;
+									Ptr--;
+									Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+									Ptr++;
+									Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+									Ptr++;
+									Ptr++;
+									Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+									Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+									Ptr--;
+									Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+									while(Mem[Ptr] > 0) {
+										Ptr--;
+										Ptr--;
+										Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+										Ptr++;
+										Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+										Ptr++;
+										Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+										while(Mem[Ptr] > 0) {
+											Ptr--;
+											Ptr--;
+											Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+											Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+											Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+											Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+											Ptr++;
+											Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+											Ptr++;
+											Ptr++;
+											Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+											Ptr--;
+											Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+											while(Mem[Ptr] > 0) {
+												Ptr++;
+												Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+												Ptr--;
+												Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+												while(Mem[Ptr] > 0) {
+													Ptr--;
+													Ptr--;
+													Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+													Ptr++;
+													Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+													Ptr++;
+													Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+													while(Mem[Ptr] > 0) {
+														Ptr--;
+														Ptr--;
+														Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+														Ptr++;
+														Ptr++;
+														Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+														while(Mem[Ptr] > 0) {
+															Ptr--;
+															Ptr--;
+															Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+															Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+															Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+															Ptr++;
+															Ptr++;
+															Ptr++;
+															Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+															Ptr--;
+															Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+															while(Mem[Ptr] > 0) {
+																Ptr--;
+																Ptr--;
+																Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																Ptr++;
+																Ptr++;
+																Ptr++;
+																Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																Ptr--;
+																Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																while(Mem[Ptr] > 0) {
+																	Ptr--;
+																	Ptr--;
+																	Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																	Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																	Ptr++;
+																	Ptr++;
+																	Ptr++;
+																	Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																	Ptr--;
+																	Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																	while(Mem[Ptr] > 0) {
+																		Ptr++;
+																		while(Mem[Ptr] > 0) {
+																			Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																		}
+																		Ptr--;
+																		Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																		while(Mem[Ptr] > 0) {
+																			Ptr--;
+																			Ptr--;
+																			Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																			Ptr++;
+																			Ptr++;
+																			Ptr++;
+																			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																			Ptr--;
+																			Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																			while(Mem[Ptr] > 0) {
+																				Ptr--;
+																				Ptr--;
+																				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																				Ptr++;
+																				Ptr++;
+																				Ptr++;
+																				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																				Ptr--;
+																				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																				while(Mem[Ptr] > 0) {
+																					Ptr--;
+																					Ptr--;
+																					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																					Ptr++;
+																					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																					Ptr++;
+																					Ptr++;
+																					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																					Ptr--;
+																					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																					while(Mem[Ptr] > 0) {
+																						Ptr--;
+																						Ptr--;
+																						while(Mem[Ptr] > 0) {
+																							Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																						}
+																						Ptr++;
+																						Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																						Ptr++;
+																						Ptr++;
+																						Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																						Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																						Ptr--;
+																						Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																						while(Mem[Ptr] > 0) {
+																							Ptr--;
+																							Ptr--;
+																							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																							Ptr++;
+																							Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																							Ptr++;
+																							Ptr++;
+																							Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																							Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																							Ptr--;
+																							Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																							while(Mem[Ptr] > 0) {
+																								Ptr--;
+																								Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																								Ptr++;
+																								Ptr++;
+																								Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																								Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+																								Ptr--;
+																								while(Mem[Ptr] > 0) {
+																									Ptr--;
+																									Ptr--;
+																									Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																									Ptr++;
+																									Ptr++;
+																									Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+																								}
+																							}
+																						}
+																					}
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			Ptr--;
+			while(Mem[Ptr] > 0) {
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				Ptr++;
+				Ptr++;
+				while(Mem[Ptr] > 0) {
+					Ptr--;
+					Ptr--;
+					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+					Ptr++;
+					Ptr++;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				}
+				Ptr--;
+				Ptr--;
+				Ptr--;
+				while(Mem[Ptr] > 0) {
+					Ptr++;
+					Ptr++;
+					Ptr++;
+					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+					Ptr--;
+					Ptr--;
+					Ptr--;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				}
+				Ptr--;
+				while(Mem[Ptr] > 0) {
+					Ptr++;
+					Ptr++;
+					Ptr++;
+					Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+					Ptr--;
+					Ptr--;
+					Ptr--;
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				}
+			}
+			Ptr++;
+			Ptr++;
+		}
+		Ptr++;
+		while(Mem[Ptr] > 0) {
+			Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+			while(Mem[Ptr] > 0) {
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				while(Mem[Ptr] > 0) {
+					Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+					Ptr--;
+				}
+			}
+			Ptr++;
+		}
+		Ptr++;
+		while(Mem[Ptr] > 0) {
+			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+			Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+			while(Mem[Ptr] > 0) {
+				Ptr--;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Ptr++;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+			}
+			Ptr++;
+		}
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		Ptr--;
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+		while(Mem[Ptr] > 0) {
+			while(Mem[Ptr] > 0) {
+				Ptr++;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Mem[Ptr] = (Mem[Ptr] + 1) % 256;
+				Ptr--;
+				Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+			}
+			Ptr--;
+		}
+		Ptr++;
+		Ptr++;
+		while(Mem[Ptr] > 0) {
+			Mem[Ptr] = (Mem[Ptr] - 1) % 256;
+			System.Console.Write((char)Mem[Ptr]);
+			Ptr++;
+		}
+	}
+}
